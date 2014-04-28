58485da2a63e4d0ffcc102cf50f6709cfdc9f339
hagino3000 <hagino.3000@gmail.com>
2014-04-28 12:34:25 +0000

    Add libfreetype

--- pre_requirements_debian.sh
+++ pre_requirements_debian.sh
@@ -1,1 +1,13 @@
-sudo apt-get install ipython-notebook python-matplotlib python-scipy python-pandas python-sympy python-nose
+sudo apt-get install \
+	ipython-notebook\
+	python-matplotlib\
+	python-scipy\
+	python-pandas\
+	python-sympy\
+	python-nose\
+	libfreetype6\
+	libfreetype6-dev\
+	python-virtualenv\
+	libzmq-dev\
+
+	
